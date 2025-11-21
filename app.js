175621437efca2f9b84802c3f1e4117e4dfcf51e
jmdavis815-web <jmdavis815@gmail.com>
2025-11-20 21:09:14 -0600

    added js

---
+++ app.js
@@ -0,0 +1,12 @@
+function convertKilometersToMiles() {
+    let kilometersInput = document.getElementById('kilometersInput').value;
+    let miles = kilometersInput / 1.60934;
+    let result = document.getElementById('result');
+
+if (isNaN(kilometersInput) || kilometersInput === '') {
+    result.style.color = "red";
+    result.innerHTML = `<h5>Please enter a valid number for kilometers.</h5>`;
+    }else {
+    result.innerHTML = `<h5>${kilometersInput} kilometers is equal to ${miles.toFixed(2)} miles.</h5>`;
+    }
+}
